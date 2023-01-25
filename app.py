a1199008f7493bfea0c07c1715fe9035c517431d
CrezyDeveloper143 <117562112+CrezyDeveloper143@users.noreply.github.com>
2023-01-25 17:00:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'https://www.youtube.com/@GreyMattersYT'
+    return 'https://youtube.com/@technical_rakesh_01'
 
 
 if __name__ == "__main__":
